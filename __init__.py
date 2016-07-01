d60113cd497f59cbc310b498f20ae4c0a2d5a08d
hugo_dev <hb11002@icloud.com>
2016-07-01 10:15:33 +0200

    Added admin command to change bot user's icon Improved admin help message formatting

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ import subprocess
 import time
 
 import discord
+import requests
 
 import helpers
 
@@ -321,12 +322,12 @@ async def cmd_help(message: discord.Message):
         for helpcommand in admin_commands:
 
             # We check if the last chunk is too will become too large or not
-            if len(admin_commands_helptext[-1] + "\n-------------------------\n\t" + "admin **" + helpcommand[
+            if len(admin_commands_helptext[-1] + "\n-------------------------\n\t" + "*admin* **" + helpcommand[
                 "command"] + "**\n" + helpcommand["helptext"] + "\n-------------------------") > 2000:
                 # We add another string to he list of messages we want to send
                 admin_commands_helptext.append("")
 
-            admin_commands_helptext[-1] += "\n-------------------------\n\t" + "admin **" + helpcommand[
+            admin_commands_helptext[-1] += "\n-------------------------\n\t" + "*admin* **" + helpcommand[
                 "command"] + "**\n" + helpcommand["helptext"] + "\n-------------------------"
 
     # How many seconds we should wait between each message
@@ -990,6 +991,58 @@ async def cmd_admin_reload_config(message: discord.Message):
 async def cmd_admin_change_icon(message: discord.Message):
     """This admin command is used to change the icon of the bot user to a specified image."""
 
+    # Checking if the message has an attachment
+    if message.attachments:
+
+        # Looping through the attachments until we find a valid one to use for an icon
+        for attachment in message.attachments:
+
+            # Checking if the message has a valid image attachment (just checking if the height and width dict keys exist)
+            if ("width" in attachment) and ("height" in attachment):
+                # We tell the user that we're changing the icon
+                await client.send_message(message.channel,
+                                          "Ok {0:s}, now changing the icon to the image you sent!".format(
+                                              message.author.mention))
+
+                # We break out of the for loop as we are done
+                # Note that as python does not change the last value of a for loop "counter" (in this case attachment) after the for loop has been exited,
+                # and that neither for loops nor if statements create new namespaces, the last value of that variable is still a regular variable after the for loop has been executed.
+                break
+
+        # This is an else statement for the for loop (no pun intended), which executes after a for loop if the for loop doesn't stop because of a break statement
+        else:
+
+            # We tell the user that they didn't attach a valid icon image to their command message
+            await client.send_message(message.channel,
+                                      "I can't do that {0:s} because you didn't attach an image for me to set my icon to.".format(
+                                          message.author.mention))
+
+            # We're done here now
+            return
+
+    # The command message didn't have any attachments
+    else:
+
+        # We tell the user that they didn't attach a valid icon image to their command message
+        await client.send_message(message.channel,
+                                  "I can't do that {0:s} because you didn't attach an image for me to set my icon to.".format(
+                                      message.author.mention))
+
+        # We're done here now
+        return
+
+    # Logging that we're going to change the icon of the bot account
+    helpers.log_info(
+        "Changing {0:s}'s icon to {1:s} because admin {2:s} ({3:s}) triggered the change icon command.".format(
+            client.user.name, attachment["url"], message.author.name, message.author.mention))
+
+    # We download the image and upload it to the discord command simultaneously
+    await client.edit_profile(avatar=requests.get(attachment["url"]).content)
+
+    # Logging and telling the user that we're done changing the icon
+    helpers.log_info("Now done changing the icon.")
+    await client.send_message(message.channel, "Now done changing the icon.")
+
 
 def is_message_command(message: discord.Message):
     """This function is used to check whether a message is trying to issue an anna-bot command"""
@@ -1064,7 +1117,9 @@ public_commands = [dict(command="invite", method=cmd_invite_link,
 admin_commands = [dict(command="broadcast", method=cmd_admin_broadcast,
                        helptext="Broadcasts a message to all the channels that anna-bot has access to."),
                   dict(command="reload config", method=cmd_admin_reload_config,
-                       helptext="Reloads the config file that anna-bot uses.")
+                       helptext="Reloads the config file that anna-bot uses."),
+                  dict(command="change icon", method=cmd_admin_change_icon,
+                       helptext="Changes the anna-bot's profile icon to an image that the user attaches to the command message.")
                   ]
 
 # The list of message ids (this list will fill and empty) that the command checker should ignore

52f149bedcbd6873f48cfeb652272876dfcdc926
hugo_dev <hb11002@icloud.com>
2017-04-05 21:58:33 +0200

    Finished usable version of anna_launcher Changed structure of bot_main Updated Readme.md
    Signed-off-by: hugo_dev <hb11002@icloud.com>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# anna-bot
+# Anna-bot
 
 [![Join the chat at https://gitter.im/anna-bot/Lobby](https://badges.gitter.im/anna-bot/Lobby.svg)](https://gitter.im/anna-bot/Lobby?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
 A discord bot created for the https://mcevalonia.com minecraft server's discord server, but usable by all discord servers.
@@ -7,7 +7,7 @@ A discord bot created for the https://mcevalonia.com minecraft server's discord 
 ### Dependencies:
 ```
 python3.5
-discord.py >= 0.16 (https://github.com/Rapptz/discord.py)
+discord.py 0.16.X (https://github.com/Rapptz/discord.py)
 youtube_dl
 ffmpeg/avconv
 aiohttp
@@ -17,8 +17,9 @@ aiodns
 
 ### Running it
 1. Clone the repository
-2. Create a discord bot account
+2. Create a discord bot account ([here](https://discordapp.com/developers/applications/me))
 3. Edit `config.json`, don't forget the bot account auth tokens :)
-4. Run `python3.5 anna_launcher.py` with appropriate flags (`python3.5 anna_launcher -rs` to run and restart if it exits)
+4. Run `python3.5 anna_launcher.py` with appropriate flags (`python3.5 anna_launcher -sr` to run and restart if it exits for some reason)
+5. Exit the bot and launcher with `CTRL+C`, or exit double-tapping `CTRL+C` if you used the `-r` flag.
 
 (Btw, the bot's name comes from a swedish pop song ;) )
--- anna_launcher.py
+++ anna_launcher.py
@@ -1,21 +1,27 @@
 #! /usr/bin/env python3.5
 import argparse
-import sys
+import asyncio
 import os
-import subprocess
+import sys
+import time
 import traceback
-
-from sys import platform as _platform
+from importlib import reload
 from importlib.util import find_spec
+from sys import platform as _platform
+
+import discord
+
+import bot_main
 
 # The modules that have to be available for anna-bot to be able to run
-required_modules = {"discord", "youtube_dl", "aiohttp", "aiodns"}
+required_modules = {"discord", "youtube_dl", "aiohttp", "aiodns", "asyncio"}
 
 if _platform == "win32":
 	# We're running on windows
 	WINDOWS = True
 else:
 	WINDOWS = False
+
 
 def parse_cli_arguments():
 	parser = argparse.ArgumentParser(description="The anna-bot launcher")
@@ -34,7 +40,7 @@ def verify_requirements():
 	for required in required_modules:
 		if find_spec(required) is None:
 			# We missed a requirement
-			print("Requirement {0} not found.".format(required))
+			launcher_log("Requirement {0} not found.".format(required))
 			return False
 
 	return True
@@ -42,32 +48,66 @@ def verify_requirements():
 
 def start_anna_bot(auto_restart: bool):
 	"""We start anna-bot by importing and launching bot_main.start_bot."""
-	
+
 	# We verify that all required modules
 	if not verify_requirements():
-		print("You do not have all requirements installed, please see the readme.") 
-	
-	# We import the anna-bot file
-	import bot_main
-	
+		launcher_log("You do not have all requirements installed, please see the readme.")
+
 	# The main loop
 	while True:
+		# We don't want to DOS our host machine
+		# During this sleep the user of anna-bot can send a CTRL+C and it won't be caught in the try-except statement
+		time.sleep(0.5)
+
 		try:
-			print("Starting anna-bot.")
+			launcher_log("Starting anna-bot file.")
 			bot_main.start_anna()
 		except Exception as e:
 			# We only catch error that are supposed to be catchable, otherwise we would've used BaseException
-			print("Anna-bot exited with exception {0}.\nTraceback: {1}".format(str(e), str(e.Traceback)))
+			e_type, e, e_traceback = sys.exc_info()
+			launcher_log("Anna-bot exited with exception. Traceback: \n{0}".format(
+				str("".join(traceback.format_exception(e_type, e, e_traceback)))))
+
+			# We check if we should restart
 			if auto_restart:
-				print("Restarting anna-bot since you used the --restart flag.")
+				launcher_log("Restarting anna-bot since you used the --restart flag.")
 				continue
-			
-			print("Exiting the launcher since anna-bot exited and you didn't use the --restart flag.")
+
+			launcher_log("Exiting the launcher since anna-bot exited and you didn't use the --restart flag.")
 			break
 		except BaseException:
-			# We got an exception that isn't supposed to be catched, so we raise it
-			print("Anna-bot exited with an un-handlable exception. Exiting launcher.")
-			raise
+			# We got an exception that isn't supposed to be catched, so we break
+			launcher_log("Anna-bot exited with an un-handlable exception. Exiting launcher.")
+			break
+
+		# We check if we should continue running anna-bot
+		if not auto_restart:
+			launcher_log("Stopping anna-bot since you didn't use the --restart flag.")
+			break
+
+		# Giving the user info about why the bot is relaunching
+		launcher_log("Restarting anna-bot since you used the --restart flag.")
+		launcher_log("Updating event loop...")
+
+		# We create and use a new event loop for the next iteration of anna-bot
+		asyncio.get_event_loop().close()
+		asyncio.set_event_loop(asyncio.new_event_loop())
+
+		# We replace the old event loop with a new one, that's the only way to "reopen" loops I was able to find
+		bot_main.helpers.actual_client.loop = asyncio.get_event_loop()
+
+		# We need to refresh the client session to not get session closed errors
+		bot_main.helpers.actual_client = discord.Client(cache_auth=False)
+
+		# We import the anna-bot file (actually we reload it, as we are running a new instance of anna)
+		reload(bot_main)
+
+		launcher_log("Done updating event loop.")
+
+
+def launcher_log(*args):
+	"""Prints a message from anna_launcher instead of regular print."""
+	print("Anna_launcher: ", *args)
 
 
 # The command line args passed to the launcher
@@ -79,15 +119,17 @@ if __name__ == '__main__':
 	# Sets current directory to the launcher's
 	os.chdir(dirname)
 	if args.start:
-		print("Anna-bot launcher initiated with flags: " + ", ".join([(pair[0]) for pair in vars(args) if pair[1]]))
-		print("Starting anna-bot...")
+		launcher_log(
+			"Anna-bot launcher initiated with flags: " + ", ".join([pair for pair in vars(args) if vars(args)[pair]]))
+		launcher_log("Starting anna-bot...")
 		try:
 			start_anna_bot(auto_restart=args.auto_restart)
 		except BaseException:
-		    e_type, e, e_traceback = sys.exc_info()
-		    print("Got exception from anna-bot, will exit. Here is the traceback: \n{0}".format(
-		        str("".join(traceback.format_exception(e_type, e, e_traceback)))))
+			e_type, e, e_traceback = sys.exc_info()
+			launcher_log("Got exception from anna-bot, will exit. Here is the traceback: \n{0}".format(
+				str("".join(traceback.format_exception(e_type, e, e_traceback)))))
 		finally:
-			print("Anna-bot launcher is now exiting.")
+			launcher_log("Anna-bot launcher is now exiting.")
 	else:
-		print("Launcher invoked without the start (-s) flag. What did you think would happen? (Use --help for info about the different flags.)")
+		launcher_log(
+			"Launcher invoked without the start (-s) flag. What did you think would happen? (Use --help for info about the different flags.)")
